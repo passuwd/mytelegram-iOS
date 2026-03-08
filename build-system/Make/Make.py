d74e16d36215f85d8227b4f8ed27f44e1f6d9673
passuwd <Damir2013K@icloud.com>
2026-03-08 17:17:44 +0000

    Update Make.py

--- build-system/Make/Make.py
+++ build-system/Make/Make.py
@@ -506,8 +506,6 @@ def resolve_configuration(base_path, bazel_command_line: BazelCommandLine, argum
     )
     if codesigning_data.aps_environment is None:
         print('Could not find a valid aps-environment entitlement in the provided provisioning profiles')
-        sys.exit(1)
-
     if bazel_command_line is not None:
         build_configuration.write_to_variables_file(bazel_path=bazel_command_line.bazel, use_xcode_managed_codesigning=codesigning_data.use_xcode_managed_codesigning, aps_environment=codesigning_data.aps_environment, path=configuration_repository_path + '/variables.bzl')
 
